6c10a4f3a343ba5fe685ca427923da4afdf777a6
Seth <sethhirsh@berkeley.edu>
2020-05-26 10:59:30 -0400

    added horseshoe

--- real_horseshoe_full_sindy_1.py
+++ real_horseshoe_full_sindy_1.py
@@ -27,30 +27,37 @@ with model_sunode:
 
     sigma = pm.Lognormal('sigma', mu=-1, sigma=0.1, shape=2)
     #sigma = pm.Lognormal('sigma', mu=0., sigma=1.0, shape=2)
-    pnlarge = pm.Laplace('pnlarge', mu=0, b=1,shape=4)
+
+    caularge = pm.HalfCauchy('caularge',1,shape=4)
+    cauone = pm.HalfCauchy('cauone',1,shape=2)
+    cau = pm.HalfCauchy('cau',1,shape=6)
+
+
+    tau = 1./3.
+    pnlarge = pm.Normal('pnlarge', mu=0, sigma=tau*1*caularge,shape=4)
     #p1 = pm.Normal('p1', mu=0, sigma=1.0)
 
-    pnone = pm.Laplace('pnone', mu=0, b=1, shape=2)
+    pnone = pm.Laplace('pnone', mu=0, b=tau*1*cauone, shape=2)
 
     #p2 = pm.Normal('p2', mu=0, sigma=1.0)
     #p3 = pm.Normal('p3', mu=0, sigma=1.0)
 
     #pn = pm.Normal('pn', mu=0, sigma=0.1, shape=4)
-    pn = pm.Laplace('pn', mu=0, b=0.1, shape=6)
+    pn = pm.Laplace('pn', mu=0, b=tau*0.1*cau, shape=6)
 
    # r  = pm.Beta('r', 1, beta)
    # xi = pm.Bernoulli('xi', r, shape=4)
 
-    xi = pm.Bernoulli('xi', 0.8, shape=6)
-    xione = pm.Bernoulli('xione', 0.8, shape=2)
+    #xi = pm.Bernoulli('xi', 0.8, shape=6)
+    #xione = pm.Bernoulli('xione', 0.8, shape=2)
 
-    xilarge = pm.Bernoulli('xilarge', 0.8,shape=4)
+    #xilarge = pm.Bernoulli('xilarge', 0.8,shape=4)
     #xip1 = pm.Bernoulli('xip1', 0.8)
-    pnsslarge = pm.Deterministic('pnsslarge',pnlarge* xilarge)
+    #pnsslarge = pm.Deterministic('pnsslarge',pnlarge* xilarge)
     #pn1 = pm.Deterministic('pn1',p1 * xip1)
-    pnssone = pm.Deterministic('pnssone',pnone* xione)
+    #pnssone = pm.Deterministic('pnssone',pnone* xione)
 
-    pnss = pm.Deterministic('pnss', pn * xi)
+    #pnss = pm.Deterministic('pnss', pn * xi)
 
     y0 = pm.Lognormal('y0', mu=pm.math.log(10), sigma=1, shape=2)
 
@@ -60,18 +67,18 @@ with model_sunode:
             'v': (y0[1], ()),
             },
             params={
-                'pnone0' : (pnssone[0], ()),
-                'pnone1' : (pnssone[1], ()),
-                'pn00': (pnsslarge[0], ()),
-                'pn11': (pnsslarge[1], ()),
-                'pn0': (pnsslarge[2], ()),
-                'pn1': (pnsslarge[3], ()),
-                'pn2': (pnss[0], ()),
-                'pn3': (pnss[1], ()),
-                'pn4': (pnss[2], ()),
-                'pn5': (pnss[3], ()),
-                'pn6': (pnss[4], ()),
-                'pn7': (pnss[5], ()),
+                'pnone0' : (pnone[0], ()),
+                'pnone1' : (pnone[1], ()),
+                'pn00': (pnlarge[0], ()),
+                'pn11': (pnlarge[1], ()),
+                'pn0': (pnlarge[2], ()),
+                'pn1': (pnlarge[3], ()),
+                'pn2': (pn[0], ()),
+                'pn3': (pn[1], ()),
+                'pn4': (pn[2], ()),
+                'pn5': (pn[3], ()),
+                'pn6': (pn[4], ()),
+                'pn7': (pn[5], ()),
                 'tmp': np.zeros(1),  # Theano wants at least one fixed parameter
             },
             rhs=predator_prey_sunode_library,
@@ -99,23 +106,24 @@ with model_sunode:
     #start['pn5'] = 0.003
     #start['pn6'] = -0.001
     #start['pn7'] = 0.012
-    start['pnone'] = [-3.726,3.388]
-    start['pnssone'] = start['pnone']
+    start['pnone'] = np.array([-3.726,3.388])
+    #start['pnssone'] = start['pnone']
     start['pnlarge'] = np.array([0.677,-1.140,-0.124,-0.066])
-    start['pnsslarge'] = start['pnlarge']
+    #start['pnsslarge'] = start['pnlarge']
 
     start['pn'] = np.array([-0.010,0.008,-0.005,0.004,-0.004,0.014])
-    start['pnss'] = start['pn']
-    start['xilarge'] = [1,1,1,1]
-    start['xione'] = [1,1]
-    start['xi'] = [1,1,1,1,1,1]
+    #start['pnss'] = start['pn']
+
+    start['caularge'] = [3,3,3,3]
+    start['cauone'] = [3,3]
+    start['cau'] = [3,3,3,3,3,3]
     start['y0'] = yobs[0,:]
 
-    trace = pm.sample(1000, tune=1000, cores=2, start=start, step_kwargs={'nuts':{'target_accept':0.95}})
+    trace = pm.sample(1000, tune=1000, cores=2,start=start, target_accept=0.95)#,step_kwargs={'nuts':{'target_accept':0.95}})
 
-    pm.backends.save_trace(trace,'real_reg_minus_5_full_sindy_9' + '.trace',model_sunode)
+    pm.backends.save_trace(trace,'real_reg_minus_5_full_sindy_16' + '.trace',model_sunode)
 
-    print('ahwejhefwggfi9')
+    print('ahwejhefwggfi15')
 print('done')
 
 
