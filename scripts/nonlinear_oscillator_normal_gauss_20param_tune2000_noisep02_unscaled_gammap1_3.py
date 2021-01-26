64bacb27f02cdfa9f70968d812375ec6d5fd5e62
Seth Hirsh <3835658+sethhirsh@users.noreply.github.com>
2021-01-25 19:58:25 -0500

    Added scripts for experiments

---
+++ scripts/nonlinear_oscillator_normal_gauss_20param_tune2000_noisep02_unscaled_gammap1_3.py
@@ -0,0 +1,181 @@
+import pymc3 as pm
+import numpy as np
+import matplotlib.pyplot as plt
+import arviz as az
+import pandas
+import sunode.wrappers.as_theano as sun
+
+
+from scipy.integrate import ode
+alpha  = -0.1
+beta= 2
+gamma=-2
+delta=-0.1
+def dX_dt(t, state,par):
+    """ Return the growth rate of fox and rabbit populations. """
+    alpha,beta,gamma,delta = par
+    return np.array([ alpha * state[0]**3 + beta*state[1]**3,
+                  gamma*state[0]**3 + delta*state[1]**3])
+
+t = np.linspace(0, 20,  40)              # time
+X0 = np.array([2, 0])                    # initials conditions: 10 rabbits and 5 foxes
+r = ode(dX_dt).set_integrator('dopri5')
+r.set_initial_value(X0, t[0])
+r.set_f_params((alpha,beta,gamma,delta))
+X = np.zeros((len(X0),len(t)))
+X[:,0] = X0
+for i, _t in enumerate(t):
+    if i == 0:
+        continue
+    r.integrate(_t)
+    X[:, i] = r.y
+
+np.random.seed(0)
+yobs = X.T + np.random.normal(size=X.T.shape) * 0.02  #np.maximum(X.T + 2*np.random.randn(*X.T.shape),1)
+times = t
+print(yobs.std(axis=0))
+yobs_norm = yobs # / yobs.std(axis=0)
+
+
+
+
+model_rh = pm.Model()
+
+d = 20
+
+slab_df = 4
+slab_scale = 2
+
+## Do Bayesian Sindy
+def nonlinear_oscillator_sunode_library(t, y, p):
+    #state = np.array([y.u**3, y.v**3])
+    #res = p.pn @ state 
+    du_dt = p.pn[0] * y.u**3 + p.pn[1] * y.v**3 + p.pn[4] * y.u + p.pn[6] * y.v + p.pn[8] * y.u**2 + p.pn[10] * y.v**2 + p.pn[12] * y.u*y.v + p.pn[14] * y.u**2 * y.v + p.pn[16] * y.v**2 * y.u + p.pn[18]
+    dv_dt = p.pn[2] * y.u**3 + p.pn[3] * y.v**3 + p.pn[5] * y.u + p.pn[7] * y.v + p.pn[9] * y.u**2 + p.pn[11] * y.v**2 + p.pn[13] * y.u*y.v + p.pn[15] * y.u**2 * y.v + p.pn[17] * y.v**2 * y.u + p.pn[19]
+    return {'u': du_dt - 1e-5 * y.u**5, 'v' : dv_dt - 1e-5 * y.v**5}
+
+with model_rh:
+
+    #sigma = pm.Lognormal('sigma', mu=-1, sigma=1, shape=2) #  pm.Gamma('sigma',1,1,shape=2) #
+    sigma = pm.Gamma('sigma',1,0.1,shape=2) 
+    #pn = pm.Laplace('pn', mu=0, b=1, shape=d)
+    
+    
+    l = pm.HalfStudentT('l', nu=1, sigma=1, shape=d)
+    tau = pm.HalfStudentT('tau', nu=1, sigma=0.1)
+    c2 = pm.InverseGamma('c2', alpha=0.5*slab_df, beta=0.5*slab_df*slab_scale**2)
+    
+    lt = pm.Deterministic('lt', pm.math.sqrt(c2)*l / pm.math.sqrt(c2 + pm.math.sqr(tau) * pm.math.sqr(l)))
+    
+    z  = pm.Normal('z', mu=0, sigma=1, shape=d)
+    pn = pm.Deterministic('pn', z*tau*lt)
+    
+    #y0 = pm.Normal('y0',mu=yobs_norm[0,:], sigma=0.01, shape=2)
+    #xi = pm.Bernoulli('xi', 0.8, shape=d
+    #pnss = pm.Deterministic('pnss', pn * xi)
+    y0 = pm.Laplace('y0', mu=0, b=1, shape=2)
+
+    y_hat, _, problem, solver, _, _ = sun.solve_ivp(
+        
+        y0={
+            'u': (y0[0], ()),
+            'v': (y0[1], ()),
+            },
+            params={
+                'pn' : (pn,d),
+                'tmp': np.zeros(1),  # Theano wants at least one fixed parameter
+            },
+            rhs=nonlinear_oscillator_sunode_library,
+            make_solver='RK45',
+            tvals=times,
+            t0=times[0],
+        )
+
+
+    import sunode
+    lib = sunode._cvodes.lib
+    lib.CVodeSetMaxNumSteps(solver._ode, 1200)
+    lib.CVodeSetMaxNumStepsB(solver._ode, solver._odeB, 1200)
+
+    uobs = pm.Normal('uobs', mu=y_hat['u'][:], sigma=sigma[0], observed=yobs_norm[:,0])
+    vobs = pm.Normal('vobs', mu=y_hat['v'][:], sigma=sigma[1], observed=yobs_norm[:,1])
+
+with model_rh:
+
+    #trace = pm.sample(2000, tune=2000, cores=2,random_seed=1,target_accept=0.9) #,start=start)
+
+    #trace = pm.sample(2000, tune=2000, cores=2, random_seed=1, step_kwargs={'nuts':{'target_accept':0.9}})
+
+    trace_rh = pm.backends.load_trace('../nonlinear_oscillator_normal_rh_20param_tune2000_noisep02_unscaled_gammap1_2.trace',model_rh)
+
+
+
+
+model_sunode = pm.Model()
+
+d = 20
+
+slab_df = 4
+slab_scale = 2
+
+with model_sunode:
+
+    #sigma = pm.Lognormal('sigma', mu=-1, sigma=1, shape=2) #  pm.Gamma('sigma',1,1,shape=2) #
+    sigma = pm.Gamma('sigma',1,0.1,shape=2) 
+    #pn = pm.Laplace('pn', mu=0, b=1, shape=d)
+    
+
+    pn  = pm.Normal('pn', mu=0, sigma=1, shape=d)
+
+    
+    #y0 = pm.Normal('y0',mu=yobs_norm[0,:], sigma=0.01, shape=2)
+    #xi = pm.Bernoulli('xi', 0.8, shape=d
+    #pnss = pm.Deterministic('pnss', pn * xi)
+    y0 = pm.Laplace('y0', mu=0, b=1, shape=2)
+
+    y_hat, _, problem, solver, _, _ = sun.solve_ivp(
+        
+        y0={
+            'u': (y0[0], ()),
+            'v': (y0[1], ()),
+            },
+            params={
+                'pn' : (pn,d),
+                'tmp': np.zeros(1),  # Theano wants at least one fixed parameter
+            },
+            rhs=nonlinear_oscillator_sunode_library,
+            make_solver='RK45',
+            tvals=times,
+            t0=times[0],
+        )
+
+
+    import sunode
+    lib = sunode._cvodes.lib
+    lib.CVodeSetMaxNumSteps(solver._ode, 1200)
+    lib.CVodeSetMaxNumStepsB(solver._ode, solver._odeB, 1200)
+    lib.CVodeSStolerances(solver._ode, 1e-6, 1e-6)
+    lib.CVodeSStolerancesB(solver._ode, solver._odeB, 1e-6, 1e-6)
+    lib.CVodeQuadSStolerancesB(solver._ode, solver._odeB, 1e-6, 1e-6)
+
+    uobs = pm.Normal('uobs', mu=y_hat['u'][:], sigma=sigma[0], observed=yobs_norm[:,0])
+    vobs = pm.Normal('vobs', mu=y_hat['v'][:], sigma=sigma[1], observed=yobs_norm[:,1])
+
+with model_sunode:
+
+    start = pm.find_MAP()
+    start['sigma'] = trace_rh['sigma'].mean(axis=0)
+    start['pn'] = trace_rh['pn'].mean(axis=0)
+    start['sigma_log__'] = np.log(start['sigma'])
+    start['y0'] = trace_rh['y0'].mean(axis=0)
+    print(start)
+
+    trace = pm.sample(2000, tune=2000, cores=2,random_seed=3,target_accept=0.9,start=start)
+
+    #trace = pm.sample(2000, tune=2000, cores=2, random_seed=1, step_kwargs={'nuts':{'target_accept':0.9}})
+
+    pm.backends.save_trace(trace,str(__file__)[:-3] + '.trace',model_sunode)
+
+
+print(__file__)
+
---
+++ scripts/synthetic_gauss_12param.py
@@ -0,0 +1,129 @@
+import pymc3 as pm
+import numpy as np
+import matplotlib.pyplot as plt
+import arviz as az
+import pandas
+import sunode.wrappers.as_theano as sun
+
+
+## Generate Data
+from scipy.integrate import ode
+alpha  = 1
+beta=0.1
+gamma=1.5
+delta=0.75 * 0.1
+def dX_dt(t, state,par):
+    """ Return the growth rate of fox and rabbit populations. """
+    alpha,beta,gamma,delta = par
+    return np.array([ alpha*state[0] -   beta*state[0]*state[1],
+                  -gamma*state[1] + delta*state[0]*state[1]])
+
+t = np.linspace(0, 24,  50)              # time
+X0 = np.array([10, 5])                    # initials conditions: 10 rabbits and 5 foxes
+r = ode(dX_dt).set_integrator('dopri5')
+r.set_initial_value(X0, t[0])
+r.set_f_params((alpha,beta,gamma,delta))
+X = np.zeros((len(X0),len(t)))
+X[:,0] = X0
+for i, _t in enumerate(t):
+    if i == 0:
+        continue
+    r.integrate(_t)
+    X[:, i] = r.y
+
+np.random.seed(0)
+yobs = X.T * np.random.lognormal(mean=0,sigma=0.1,size=X.T.shape)  #np.maximum(X.T + 2*np.random.randn(*X.T.shape),1)
+times = t
+print(yobs.std(axis=0))
+yobs_norm = yobs / yobs.std(axis=0)
+
+
+
+## Do Bayesian Sindy
+def predator_prey_sunode_library(t, y, p):
+    du_dt = p.pn[0] * y.u + p.pn[2] * y.v + p.pn[4] * y.u * y.v + p.pn[6] * y.u**2 + p.pn[8]* y.v**2 + p.pn[10] - 1e-5 * y.u**3
+    dv_dt = p.pn[1] * y.u + p.pn[3] * y.v + p.pn[5] * y.u * y.v + p.pn[7] * y.u**2 + p.pn[9]*y.v**2 + p.pn[11] - 1e-5 * y.v**3
+    return {'u': du_dt, 'v' : dv_dt}
+
+
+d = 12
+
+model_sunode = pm.Model()
+
+with model_sunode:
+
+    sigma = pm.Lognormal('sigma', mu=-1, sigma=0.1, shape=2)
+    pn = pm.Normal('pn', mu=0, sigma=1, shape=d)
+    y0 = pm.Lognormal('y0', mu=pm.math.log(1), sigma=1, shape=2)
+
+    y_hat, _, problem, solver, _, _ = sun.solve_ivp(
+        
+        y0={
+            'u': (y0[0], ()),
+            'v': (y0[1], ()),
+            },
+            params={
+                'pn' : (pn,d),
+                'tmp': np.zeros(1),  # Theano wants at least one fixed parameter
+            },
+            rhs=predator_prey_sunode_library,
+            make_solver='RK45',
+            tvals=times,
+            t0=times[0],
+        )
+
+
+    import sunode
+    lib = sunode._cvodes.lib
+    lib.CVodeSetMaxNumSteps(solver._ode, 1200)
+    lib.CVodeSetMaxNumStepsB(solver._ode, solver._odeB, 1200)
+    lib.CVodeSStolerances(solver._ode, 1e-6, 1e-6)
+    lib.CVodeSStolerancesB(solver._ode, solver._odeB, 1e-6, 1e-6)
+    lib.CVodeQuadSStolerancesB(solver._ode, solver._odeB, 1e-6, 1e-6)
+
+    uobs = pm.Lognormal('uobs', mu=pm.math.log(y_hat['u'][:]), sigma=sigma[0], observed=yobs_norm[:,0])
+    vobs = pm.Lognormal('vobs', mu=pm.math.log(y_hat['v'][:]), sigma=sigma[1], observed=yobs_norm[:,1])
+
+with model_sunode:
+
+    start = pm.find_MAP()
+
+    # Initialize parameters with least squares and all other values with MAP
+    inp = yobs_norm
+    u = inp[:,0]
+    v = inp[:,1]
+
+    θ = np.array([u,v,u*v,u**2,v**2,np.ones(u.shape)]).T
+
+    import pysindy as ps
+    from pysindy.differentiation import SmoothedFiniteDifference
+    sfd = SmoothedFiniteDifference(smoother_kws={'window_length': 5})
+    dx = sfd(inp)
+
+    guess = np.linalg.lstsq(θ,dx)[0]
+    
+    print('Initialization')
+    print(guess)
+
+    start['pn'] = guess.flatten()
+    start['pnss'] = start['pn']
+    start['xi'] = np.ones(start['pn'].shape).astype(int)
+    start['y0'] = yobs_norm[0,:]
+    start['y0_log__'] = np.log(start['y0'])
+
+    trace = pm.sample(4000, tune=2000, cores=2, start=start,random_seed=0,target_accept=0.95)
+
+    pm.backends.save_trace(trace,'synthetic_gauss_12param' + '.trace',model_sunode)
+
+# If you want to plot it make sure you can display graphics when you ssh
+#plt.figure()
+#plt.traceplot(trace)
+#plt.savefig('synthetic_reg_minus_5_full_sindy_10.png')
+
+import sys
+print(sys.argv[0])
+print('done')
+
+
+    
+
